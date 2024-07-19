3ea7025e6bfa59dac4137b7d4db6a8ed607ef4cb
Akash Choudhary <akashchoudhary436@gmail.com>
2024-07-19 19:38:04 +0530

    upd

---
+++ src/main/java/com/fastshare/FastShare/FastShareApplication.java
@@ -0,0 +1,13 @@
+package com.fastshare.FastShare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastShareApplication.class, args);
+	}
+
+}
